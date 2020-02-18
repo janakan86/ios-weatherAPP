3aaed9e54cac7ebf73e0c2ef802081bdec8140d1
Jana <janakan86@gmail.com>
2020-02-19 08:17:40 +1100

    Restructure code into groups

---
+++ weatherApp/ViewControllers/SearchViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SearchViewController.swift
+//  weatherApp
+//
+//  Created by K Janakan on 13/2/20.
+//  Copyright © 2020 K Janakan. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class SearchViewController: UIViewController {
+    
+    var container: NSPersistentContainer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
